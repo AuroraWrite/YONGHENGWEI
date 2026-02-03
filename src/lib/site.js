ec1fe5132bc9181353f020669c15af3b5a383c96
aurora <sjwang10@163.com>
2026-02-03 16:13:16 +0800

    电话

--- src/lib/site.js
+++ src/lib/site.js
@@ -2,7 +2,7 @@ export const SITE = {
   name: 'YONGHENGWEI',
   company: '吉林省永恒威商贸有限公司',
   contact: '干友良',
-  phone: '18689990523',
+  phone: '+86 18689990523',
   address: '长春市汽车开发区明达路达兴东区12栋三单元206室',
   mapSearchUrl:
     'https://www.amap.com/search?query=%E9%95%BF%E6%98%A5%E5%B8%82%E6%B1%BD%E8%BD%A6%E5%BC%80%E5%8F%91%E5%8C%BA%E6%98%8E%E8%BE%BE%E8%B7%AF%E8%BE%BE%E5%85%B4%E4%B8%9C%E5%8C%BA12%E6%A0%8B%E4%B8%89%E5%8D%95%E5%85%83206%E5%AE%A4',
